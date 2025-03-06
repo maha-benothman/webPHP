49a76f45d3135f74466ae50f2771f22187cda7f7
Maha <118288155+maha-benothman@users.noreply.github.com>
2025-03-06 14:05:57 +0100

    Create index.php
    adding index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Affichage d'un message de bienvenue
+echo "<h1>Bienvenue sur mon site PHP !</h1>";
+
+// Affichage de la date et de l'heure actuelles
+echo "<p>Nous sommes le " . date('d/m/Y') . " et il est " . date('H:i:s') . "</p>";
+?>
